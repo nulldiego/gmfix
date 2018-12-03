da0671136155c1e5b38c6148e0e6c127f5c2a120
Diego Sánchez <diegosanchezlamas@gmail.com>
2018-12-03 20:31:26 +0100

    bulk delete url mapping

--- gmfix/urls.py
+++ gmfix/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'playlists/$', views.playlists, name='playlists'),
     url(r'ajax/backup/$', views.backup, name='backup'),
+    url(r'ajax/delete/$', views.delete, name='delete'),
     url(r'ajax/restore$', views.restore, name='restore'),
     url(r'ajax/backup_all/$', views.backup_all, name='backup_all'),
     url(r'ajax/setlist/$', views.setlist, name='setlist'),
